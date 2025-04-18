3f97ea4a732789ff78d1c8b7f7f4b7a5159fb8b5
veydant <veydant.1008@gmail.com>
2025-04-18 12:12:24 +0530

    Updated service worker and made changes

--- main.js
+++ main.js
@@ -1,25 +1,23 @@
 if ('serviceWorker' in navigator && 'SyncManager' in window) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('sw.js')
-        .then(reg => {
-          console.log('Service Worker registered!', reg);
-  
-          // Register sync event if sync is supported
-          if ('sync' in reg) {
-            reg.sync.register('sync-order') // Register background sync with tag 'sync-order'
-              .then(() => {
-                console.log('Sync event registered');
-              })
-              .catch(err => {
-                console.error('Sync registration failed:', err);
-              });
-          }
-        })
-        .catch(err => {
-          console.error('Service Worker registration failed:', err);
-        });
-    });
-  } else {
-    console.error('Service Worker or SyncManager not supported.');
-  }
-  
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('sw.js')
+      .then(reg => {
+        console.log('Service Worker registered!', reg);
+
+        if ('sync' in reg) {
+          reg.sync.register('sync-order')
+            .then(() => {
+              console.log('Sync event registered');
+            })
+            .catch(err => {
+              console.error('Sync registration failed:', err);
+            });
+        }
+      })
+      .catch(err => {
+        console.error('Service Worker registration failed:', err);
+      });
+  });
+} else {
+  console.error('Service Worker or SyncManager not supported.');
+}
